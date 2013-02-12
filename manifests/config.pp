c67e3da1866e9866c4cb38a0c2fe0a790ce8551b
kim0 <email.ahmedkamal@googlemail.com>
2013-02-12 21:09:02 +0200

    Avoid templating config file, to keep in-sync with new versions of kibana Add kibana_host as a parameter

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,14 @@
-class kibana::config {
-  file { '/opt/kibana/KibanaConfig.rb':
-    ensure  => present,
-    content => template('kibana/opt/kibana/KibanaConfig.rb.erb')
+class kibana::config (
+  # Avoid templating config file, to keep in-sync with new versions of kibana
+  # Instead, we edit config file shipped with kibana
+
+  # Config file needs IP address between single quotes!
+  # TODO: Add a "quoted IP validator"
+  $kibana_host = "'0.0.0.0'",
+) {
+  file_line { '/opt/kibana/KibanaConfig.rb':
+      path  => '/opt/kibana/KibanaConfig.rb',
+      match => '\s+KibanaHost = .*$',
+      line  => "  KibanaHost = $kibana_host",
   }
 }
