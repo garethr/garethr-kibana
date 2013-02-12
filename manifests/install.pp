d27536e2c1ffbd9e67c5675bc5d089daa7f5d884
kim0 <email.ahmedkamal@googlemail.com>
2013-02-12 21:12:50 +0200

    Ensure systems packages are installed (Ubuntu 12.04), install latest Kibana always

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,25 +2,34 @@ class kibana::install {
   wget::fetch { 'download_kibana':
     source      => 'http://github.com/rashidkpc/Kibana/tarball/kibana-ruby',
     destination => '/usr/local/src/kibana-ruby.tar.gz',
-    before      => Exec['untar_kibana'],
-  }
-
-  exec { 'untar_kibana':
-    command => 'tar xvf /usr/local/src/kibana-ruby.tar.gz',
-    cwd     => '/opt',
-    creates => "/opt/rashidkpc-Kibana-${kibana::git_hash}",
-    path    => '/bin',
-    before  => File['/opt/kibana'],
+    before      => File['/opt/kibana'],
   }
 
   file { '/opt/kibana':
-    ensure => link,
-    target => "/opt/rashidkpc-Kibana-${kibana::git_hash}",
+    ensure => directory,
+    before => Exec['untar_kibana'],
+  }
+
+  exec { 'untar_kibana':
+    command => 'tar --strip-components 1 -xzf /usr/local/src/kibana-ruby.tar.gz',
+    cwd     => '/opt/kibana',
+    creates => "/opt/kibana/Rakefile",  # Dirty!
+    path    => '/bin',
+    before  => Exec['bundle_kibana'],
+  }
+
+  $ruby_pkgs = $operatingsystem ? {
+    'Ubuntu'  => [ "ruby-bundler", "rubygems" ],
+    default   => [ "ruby-bundler", "rubygems" ],
+  }
+
+  package { $ruby_pkgs:
+    ensure => "installed",
     before => Exec['bundle_kibana'],
   }
 
   exec { 'bundle_kibana':
-    command => 'bundle install --path vendor',
+    command => '/usr/bin/bundle install --path vendor',
     cwd     => '/opt/kibana',
     creates => '/opt/kibana/vendor',
   }
